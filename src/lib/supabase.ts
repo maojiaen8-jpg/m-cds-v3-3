eb3c68a762a182f0efc51815440d05730a2a68a4
maojiaen8-jpg <maojiaen8@gmail.com>
2026-04-22 23:24:39 +0800

    Fix database and dependencies

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 这是你的 Supabase 项目地址
+// 1. 项目地址
 const supabaseUrl = 'https://lhrkslumgcuilgqdcytm.supabase.co'
 
-// 这是你刚才复制的那个很长的 Anon Key
+// 2. 你的 Anon Key
 const supabaseAnonKey = 'sb_publishable_bm-pvrMUs-Iv0CpkkVURag_02Z6_BEd'
 
+// 3. 创建并导出客户端
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
